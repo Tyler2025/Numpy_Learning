d10573c7268a201f16dc47cad066981b037a4f9e
Tyler <1930417368@qq.com>
2020-05-24 00:20:05 +0800

    完整创建数组

--- 1_1_array_create.py
+++ 1_1_array_create.py
@@ -1,6 +1,30 @@
-#coding = gbk
+#coding:utf-8
+
 import numpy as np
+import random
 
-
+#创建数组
 t1 = np.array([1,2,3])
 print(t1)
+
+t2 = np.array(range(1,10))
+print(t2)
+
+t3 = np.arange(1,10,2)
+print(t3)
+print(t3.dtype)
+
+#指定改变数据类型
+t4 = np.arange(1,5,dtype="u1")
+print(t4)
+print(t4.dtype)
+
+t4 = t4.astype("u2")#调用方法后要再次赋值
+print(t4.dtype)
+
+#Numpy的小数
+t5 = [random.random() for i in range(10)]
+print(t5)
+
+t5 = np.round(t5,3)#保留3位
+print(t5)
